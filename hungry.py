b60f8c68f16f60efb3e63af5e8864fa6ca5fac54
Toshi Rawka <torawka@apac.corpdir.net>
2022-09-23 16:30:17 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,8 @@ hungry = input("Are you hungry?(Yes/No)")
 if hungry == "Yes":
     print("Eat Samosa")
 else:
-    print("Go for walk.")
+    thirsty = input("Are you thirsty?(Yes/No)")
+    if thirsty == "Yes":
+        print("Drink water")
+    else:
+        print("Go for a walk!!")
